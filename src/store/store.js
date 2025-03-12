8f87e51eee8754cba3b99e0c97a654c8e232b29f
BhavaniReddy17 <bhavanireddy228@gmail.com>
2025-03-12 12:30:12 +0530

    change in import

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit';
-import authReducer from './AuthSliceuthSlice';
+import authReducer from './AuthSlice';
 
 const store = configureStore({
     reducer: {
